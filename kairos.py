b219fdae41c0accdc6d28b9b1d418c67bde60ea7
gilofos <akatseras@gmail.com>
2026-02-05 05:15:19 +0200

    Update kairos.py

--- kairos.py
+++ kairos.py
@@ -1,18 +1,25 @@
 import requests
 
+# Η ΔΙΕΥΘΥΝΣΗ ΤΗΣ ΦΟΡΜΑΣ ΣΟΥ
 URL = "https://docs.google.com/forms/d/e/1FAIpQLSeVcHvv0c3pR53N5X8NP7sg2KY4hLxPAZXBHyhM1cxh4_KYbQ/formResponse"
-ENTRY = "entry.1147714150" 
 
 def update_weather():
+    # Ο ΚΑΙΡΟΣ ΓΙΑ ΤΟΝ ΓΗΛΟΦΟ
     weather_url = "https://api.openweathermap.org/data/2.5/weather?lat=39.91&lon=21.81&appid=154abadcd6dbf332847ef2f672a9793c&units=metric"
     try:
         res = requests.get(weather_url).json()
         temp = res['main']['temp']
         keimeno = f"{temp} C"
-        requests.post(URL, data={ENTRY: keimeno})
-        print(f"Efyge: {keimeno}")
+        
+        # Στέλνουμε τη θερμοκρασία στο Excel
+        data = {
+            "entry.1147714150": keimeno
+        }
+        
+        requests.post(URL, data=data)
+        print(f"Efyge h thermokrasia: {keimeno}")
     except:
-        print("Sfalma")
+        print("Sfalma sth lhpsh")
 
 if __name__ == "__main__":
     update_weather()
